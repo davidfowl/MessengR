274327058df67c5743d168397831b705378a6d1e
David Fowler <davidfowl@gmail.com>
2012-07-13 03:34:00 -0700

    Provide way to login via forms auth (via basic auth end point).

---
+++ MessengR.Client/LoginHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+namespace MessengR.Client
+{
+    public static class LoginHelper
+    {
+        public static Cookie Login(string url, string userName, string password)
+        {
+            // We're going to login and retrieve the auth token
+            var uri = new Uri(new Uri(url, UriKind.Absolute), "Account/Login.ashx");
+            var webRequest = (HttpWebRequest)HttpWebRequest.Create(uri);
+            webRequest.Headers["Authorization"] = "Basic " + GetAuthHeader(userName, password);
+            webRequest.CookieContainer = new CookieContainer();
+
+            using (var response = (HttpWebResponse)webRequest.GetResponse())
+            {
+                // Return the auth cookie
+                return response.Cookies[".ASPXAUTH"];
+            }
+        }
+
+        private static string GetAuthHeader(string userName, string password)
+        {
+            string authorizationHeader = userName + ":" + password;
+            authorizationHeader = Convert.ToBase64String(Encoding.Default.GetBytes(authorizationHeader));
+            return authorizationHeader;
+        }
+    }
+}
