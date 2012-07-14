8f8f6a9a77b92e044814deeb921cc6cb3502e672
Henrik Andersson <alfhenrik@gmail.com>
2012-07-14 16:24:49 +1000

    Login page added
    Wired login up to authenticate against web site

---
+++ MessengR.Client/Login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MessengR.Client
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var authCookie = LoginHelper.Login("http://localhost:60031/", txtUsername.Text, txtPassword.Password);
+            if(authCookie != null)
+            {
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+        }
+    }
+}
