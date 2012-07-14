7bed326cf6718d7f503c4950298501ed7aa78bd8
Henrik Andersson <alfhenrik@gmail.com>
2012-07-14 22:42:20 +1000

    Move views to their own folder and add icon

---
+++ MessengR.Client/Views/Login.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MessengR.Client
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var authResult = LoginHelper.Login(ConfigurationManager.AppSettings["HostURL"], txtUsername.Text, txtPassword.Password);
+            if(authResult.StatusCode == HttpStatusCode.OK)
+            {
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                if(!string.IsNullOrEmpty(authResult.Message))
+                {
+                    txtError.Text = authResult.Message;
+                    txtError.Visibility = Visibility.Visible;
+                }
+            }
+        }
+    }
+}
