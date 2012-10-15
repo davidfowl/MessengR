e3051347123ce77292c886b7db15983c36d9d25f
David Fowler <davidfowl@gmail.com>
2012-10-14 18:16:47 -0700

    Remove usings.

--- MessengR/Hubs/Chat.cs
+++ MessengR/Hubs/Chat.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Security;
 using MessengR.Models;
